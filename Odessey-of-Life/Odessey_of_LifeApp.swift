93a15d4f809216ffe9516f91f05c1b12f2701c14
Roshan <1roshanekka@gmail.com>
2024-03-25 22:22:31 +0530

    Initial Commit

---
+++ Odessey-of-Life/Odessey_of_LifeApp.swift
@@ -0,0 +1,32 @@
+//
+//  Odessey_of_LifeApp.swift
+//  Odessey-of-Life
+//
+//  Created by Roshan Ekka on 25/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Odessey_of_LifeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
